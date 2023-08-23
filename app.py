3034552a17f2547d9c5beb42edd07040cc43752d
Hao <tomaseuwillieom@gmail.com>
2023-08-24 00:31:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.secret_key = "fasfafs"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
     
     return 'àdsfdsfgsd'
